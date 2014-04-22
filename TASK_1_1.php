62228ad3622c7bb3376799042216ab40836bea7d
Ruslan Smyk <winrilz@gmail.com>
2014-04-22 10:03:43 -0700

    up task1_1 test

--- TASK_1_1.php
+++ TASK_1_1.php
@@ -1,1 +1,20 @@
-TEST
+<?php
+
+//с кириллицей проблемы
+
+$str = "MYY_-_"; 
+$len = mb_strlen($str);
+
+if($len){
+	$m = array();
+	
+	for($i=0; $i<$len; $i++){
+		if(!isset($m[$str{$i}]))
+			$m[$str{$i}] = 1;
+		else
+			$m[$str{$i}]++;
+	}
+	
+	print_r($m);
+}else
+	echo 'Enter good str!';
